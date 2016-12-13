86c8748da7bf86a25a04f0c558568118076ddbfa
Brian Doane <brianmdoane@gmail.com>
2016-12-13 12:41:33 -0600

    added basic sticky footer

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const path = require('path')
 
 const port = process.env.PORT || 3000
 app.set('port', port)
+
+// MIDDLEWARE 
 // Setting static directory.  path.join to avoid syntactic issues of direct restructures on different operating systems.
 app.use(express.static(path.join(__dirname, 'public')))
 
