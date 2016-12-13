cc3c131638d4f3ba620d480660c0f4dfccd84b13
Brian Doane <brianmdoane@gmail.com>
2016-12-12 21:47:15 -0600

    test route hitting local host

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Eat-MEAN
+
+###Simple MEAN app to pick favorite restaurants.
--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express()
 const port = process.env.PORT || 3000
 app.set('port', port)
 
+app.get('/', (req, res) => {
+	res.send('Hello Nurse!')
+})
+
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`)
 })
