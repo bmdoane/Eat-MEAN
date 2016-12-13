5c13e50f4fbf85d39594d525f153a79a75f18915
Brian Doane <brianmdoane@gmail.com>
2016-12-13 12:12:23 -0600

    installed bootswatch to add theme and jquery, for footer

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 
 const express = require('express')
 const app = express()
+const path = require('path')
 
 const port = process.env.PORT || 3000
 app.set('port', port)
+// Setting static directory.  path.join to avoid syntactic issues of direct restructures on different operating systems.
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-	res.send('Hello Nurse!')
+
 })
 
 app.listen(port, () => {
