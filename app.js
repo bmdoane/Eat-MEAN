30ec4bd01bd08ec4d6dbe46f3e5a4303281e6a8f
Brian Doane <brianmdoane@gmail.com>
2016-12-13 12:52:42 -0600

    middleware using req.method and req.url

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const path = require('path')
 const port = process.env.PORT || 3000
 app.set('port', port)
 
-// MIDDLEWARE 
+// MIDDLEWARE
+// This shows what is method is being called and what endpoint loading a page 
+app.use((req, res, next) => {
+	console.log(req.method, req.url)
+	next()
+})
 // Setting static directory.  path.join to avoid syntactic issues of direct restructures on different operating systems.
 app.use(express.static(path.join(__dirname, 'public')))
 
