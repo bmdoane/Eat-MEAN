f3bb52878411a94fc76fb9d8c960d19ba09350c7
Brian Doane <brianmdoane@gmail.com>
2017-01-08 23:14:23 -0600

    added another update to create _id for just one review for one restaurant, also to be executed in mongo shell

--- api/data/reviewIdGen.js
+++ api/data/reviewIdGen.js
@@ -1,5 +1,6 @@
-// Used to create review._id in Mongo shell for imported data not created by Mongoose methods.  Easier to create here and cut and paste into shell.
+// Used to create review._id in Mongo shell for imported data not created by Mongoose methods.  Easier to create here and cut and paste into shell to execute.
 
+// To create _id for the first review of each restaurant
 // db.restaurants.update(
 // 	{},
 // 	{
@@ -11,3 +12,15 @@
 // 		multi: true
 // 	}
 // )
+
+// Create _id for one restaurant with a second review
+db.restaurants.update(
+	{
+		name: "Loveless Cafe"
+	},
+	{
+		$set: {
+			"reviews.1._id" : ObjectId()
+		}
+	}
+)
