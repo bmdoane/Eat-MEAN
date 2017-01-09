35ba0af0ef04f04f5b0d2025df9746b775746a66
Brian Doane <brianmdoane@gmail.com>
2017-01-08 23:32:28 -0600

    used mongoose to complete route to get subdocument of reviews from database

--- api/controllers/reviewsCtrl.js
+++ api/controllers/reviewsCtrl.js
@@ -21,5 +21,18 @@ module.exports.reviewsGetAll = (req, res) => {
 
 // Get a single review
 module.exports.reviewsGetOne = (req, res) => {
+	const restaurantId = req.params.restaurantId
+	const reviewId = req.params.reviewId
+	console.log(`Get reviewId ${reviewId} for restaurantId ${restaurantId}`)
 	
+	Restaurant
+		.findById(restaurantId)
+		.select('reviews')
+		.exec((err, restaurant) => {
+			console.log('Returned restaurant', restaurant)
+			let review = restaurant.reviews.id(reviewId) 
+			res
+				.status(200)
+				.json(review)			
+		})	
 }
