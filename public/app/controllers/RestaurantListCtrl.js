352bd16735f7ff9e010e54cf55e24b5f883219e3
Brian Doane <brianmdoane@gmail.com>
2017-01-12 14:44:57 -0600

    able to log restaurant data through restaurantListCtrl to console from database

--- public/app/controllers/RestaurantListCtrl.js
+++ public/app/controllers/RestaurantListCtrl.js
@@ -2,7 +2,10 @@
 
 app.controller('RestaurantListCtrl', RestaurantListCtrl)
 
-function RestaurantListCtrl() {
+function RestaurantListCtrl($http) {
 	let vm = this
 	vm.title = 'EatMEAN App'
+	$http.get('/api/restaurants').then((response) => {
+		console.log("response", response)
+	})
 }
