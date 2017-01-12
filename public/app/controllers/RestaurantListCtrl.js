960becde5a0aba2b547b854fadcd8133068768fe
Brian Doane <brianmdoane@gmail.com>
2017-01-12 14:51:03 -0600

    restaurant names listed on html page using ng-repeat.  Count from backend increased at client side .get method

--- public/app/controllers/RestaurantListCtrl.js
+++ public/app/controllers/RestaurantListCtrl.js
@@ -5,7 +5,8 @@ app.controller('RestaurantListCtrl', RestaurantListCtrl)
 function RestaurantListCtrl($http) {
 	let vm = this
 	vm.title = 'EatMEAN App'
-	$http.get('/api/restaurants').then((response) => {
-		console.log("response", response)
+	$http.get('/api/restaurants?count=10').then((response) => {
+		// console.log("response", response)
+		vm.restaurants = response.data
 	})
 }
