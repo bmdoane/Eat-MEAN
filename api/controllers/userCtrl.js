c249bc0a88e16c658a6d33cecf49f671fc421806
Brian Doane <brianmdoane@gmail.com>
2017-01-16 16:12:10 -0600

    created a userCtrl.js file and required it along with route creation in index.js

---
+++ api/controllers/userCtrl.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const mongoose = require('mongoose')
+let User = mongoose.model('User')
+
+module.exports.register = (req, res) => {
+
+}
+
+module.exports.login = (req, res) => {
+	
+}
--- api/routes/index.js
+++ api/routes/index.js
@@ -4,6 +4,7 @@ const { Router } = require('express')
 const router = Router()
 const { restaurantsGetAll, restaurantsGetOne, restaurantsAddOne, restaurantsUpdateOne, restaurantsDeleteOne } = require('../controllers/restaurantsCtrl')
 const { reviewsGetAll, reviewsGetOne, reviewsAddOne, reviewsUpdateOne, reviewsDeleteOne } = require('../controllers/reviewsCtrl')
+const { register, login } = require('../controllers/userCtrl')
 
 router
 	.route('/restaurants')
@@ -26,6 +27,15 @@ router
 	.route('/restaurants/:restaurantId/reviews/:reviewId')
 	.get(reviewsGetOne)	
 	.put(reviewsUpdateOne)
-	.delete(reviewsDeleteOne)	
+	.delete(reviewsDeleteOne)
+
+// Authentication
+router
+	.route('/users/register')
+	.post(register)	
+
+router
+	.route('/users/login')
+	.post(login)			
 		
 module.exports = router
