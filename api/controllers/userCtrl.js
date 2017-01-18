9a2ba8999d2a5cd0ec038195b39712966446a156
Brian Doane <brianmdoane@gmail.com>
2017-01-18 14:43:37 -0600

    installed bcrypt and added to userCtrl.js to hash passwords when users register

--- api/controllers/userCtrl.js
+++ api/controllers/userCtrl.js
@@ -2,6 +2,7 @@
 
 const mongoose = require('mongoose')
 let User = mongoose.model('User')
+let { hashSync, genSaltSync, compareSync } = require('bcrypt-nodejs')
 
 module.exports.register = (req, res) => {
 	console.log('Registering user')
@@ -14,7 +15,8 @@ module.exports.register = (req, res) => {
 		.create({
 			userName: userName,
 			name: name,
-			password: password
+			// Hashing pw with bcrypt
+			password: hashSync(password, genSaltSync(10))
 		}, (err, user) => {
 			if (err) {
 				console.log(err)
@@ -44,9 +46,14 @@ module.exports.login = (req, res) => {
 				res
 					.status(400).json(err)
 			} else {
-				console.log('User found', user)
-				res
-					.status(200).json(user)				
+				if (compareSync(password, user.password)) {
+					console.log('User found', user)
+					res
+						.status(200).json(user)									
+				} else {
+					res
+						.status(401).json('Unauthorized')
+				}
 			}
 		})		
 }
