aba65d4851c479bb1cf4b95de20b85be6ed66f21
Brian Doane <brianmdoane@gmail.com>
2017-01-18 14:13:44 -0600

    able to register user and add to database

--- api/controllers/userCtrl.js
+++ api/controllers/userCtrl.js
@@ -4,6 +4,28 @@ const mongoose = require('mongoose')
 let User = mongoose.model('User')
 
 module.exports.register = (req, res) => {
+	console.log('Registering user')
+
+	let userName = req.body.userName
+	let name = req.body.name || null
+	let password = req.body.password
+
+	User
+		.create({
+			userName: userName,
+			name: name,
+			password: password
+		}, (err, user) => {
+			if (err) {
+				console.log(err)
+				res
+					.status(400).json(err)
+			} else {
+				console.log('User created', user)
+				res
+					.status(201).json(user)
+			}
+		})
 
 }
 
